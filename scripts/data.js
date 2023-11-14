9dda73f8f70a843d348f5f2b4ac4b6e10014f44b
mshashank559 <115879041+mshashank559@users.noreply.github.com>
2023-11-15 02:03:16 +0530

    Add files via upload

---
+++ scripts/data.js
@@ -0,0 +1,69 @@
+const github = "https://github.com/mshashank559/";
+const email = "mailto:mshashankmishra559@gmail.com";
+var today = new Date();
+
+about = [
+  "<br>",
+  "Hey, I'm Shashank Mishra! 👋",
+  "<br>",
+  "I'm a student at SRM University, Kattankulathur,.",
+  "I love to build things that are useful and can have a positive impact on other people's lives.",
+  "Having said that, I'm always ready to work on quirky projects",
+  "I am always ready to explore new things and collaborate with others.",
+  "Gathering a lot of knowledge and insights.",
+  "Outside of the professional space, I love playing games and casual streaming.",
+  "<br>",
+  "Hope you explore and enjoy this website! 🙏🏻",
+  "<br>",
+];
+
+banner = [
+  "<br>",
+  '<span class = "index">' + String(today) + "</span>",
+  '<span class="index">Shashank Mishra </span>',
+  "<br>",
+  
+"███████╗██╗  ██╗ █████╗ ███████╗██╗  ██╗ █████╗ ███╗   ██╗██╗  ██╗",
+"██╔════╝██║  ██║██╔══██╗██╔════╝██║  ██║██╔══██╗████╗  ██║██║ ██╔╝",
+"███████╗███████║███████║███████╗███████║███████║██╔██╗ ██║█████╔╝ ",
+"╚════██║██╔══██║██╔══██║╚════██║██╔══██║██╔══██║██║╚██╗██║██╔═██╗ ",
+"███████║██║  ██║██║  ██║███████║██║  ██║██║  ██║██║ ╚████║██║  ██╗",
+"╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝",
+                                                                  
+
+  "<br>",
+  '<span class="color2">Welcome to my Portfolio Website..</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+  // '<span class="color2"></span>',
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '👦🏻  <span class="command">About</span>          About Me',
+  '🌏  <span class="command">Social</span>         Display social networks',
+  '🏗  <span class="command">Projects</span>       View coding projects',
+  '📧  <span class="command">Email</span>          Reach me through email',
+  '🧹  <span class="command">Clear</span>          Clear the terminal',
+  '❓  <span class="command">Help</span>           Open the help menu',
+  '🕰  <span class="command">History</span>        View command history',
+  '👋  <span class="command">Exit</span>           Close the tab and exit',
+  "<br>",
+];
+
+social = [
+  "<br>",
+  "Enter the name of any of the following or simply click the links...",
+  "<br>",
+  'Github         <a href="' +
+    github +
+    '" target="_blank">github/mshashank559' +
+    "</a>",
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  'Click the following links to access all my projects: <a href = "https://github.com/mshashank559/" target="_blank">(Github)</a>',
+  "<br>",
+];
